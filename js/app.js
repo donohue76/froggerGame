3c6ae29cedd6e9f72f8f312ddeebd959f40d8eee
David Patrick Donohue <donohue76@hotmail.com>
2018-06-06 19:39:31 -0700

    completed final project code

--- js/app.js
+++ js/app.js
@@ -54,16 +54,100 @@ Player.prototype.render = function() {
     displayScoreLevel(score, gameLevel);
 }:
 
+Player.prototype.handleInput = function(keyPress) {
+    if (keyPress == 'left') {
+        player.x -= player.speed;
+    }
+    if (keyPress == 'right') {
+        player.x = += player.speed;
+    }
+    if (keyPress == 'up') {
+        player.y += player.speed - 20;
+    }
+    if (keyPress == 'down') {
+        player.y = player.speed - 20;
+    }
+    console.log('keyPress is: ' + keyPress);
+};
+// Function to display player's score
+let displayScoreLevel = function(aScore, aLevel) {
+    let canvas = document.getElementsByTagName('canvas');
+    let firstCanvasTag = canvas[0];
+
+    // add player score and level to div element created
+    scoreLevelDiv.innerHTML = 'Score: ' + sScore + ' / ' + 'Level: ' + aLevel;
+    document.body.insertBefore(scoreLevelDiv, firstCanvasTag[0]);
+};
+
+let checkCollision = function(anEnemy) {
+    // check for collision between enemy and player
+    if (player.y + 131 >= anEnemy.y + 90 && player.x + 25 <= anEnemy.x + 88 && player.y + 23 <= anEnemy.y + 135 && player.x + 76 >= anEnemy.x + 11) {
+        console.log('collided');
+        player.x = 202.5;
+        player.y = 383;
+    }
+
+    // check for player reaching top of canvas and winning the game, if player wins, add 1 to the score and level.
+    // pass score as an argument to the increasesDifficulty function
+    if (player.x + 63 <= 0) {
+        player.x = 202.5;
+        player.y = 383;
+        console.log('You made it!');
+
+        ctx.fillsStyle = 'white';
+        ctx.fillRect(0, 0, 505, 171);
+
+        score += 1;
+        gameLevel += 1;
+        console.log('Current Score: ' + score + ', Current Level: ' + gameLevel);
+        increasesDifficulty(score);
+    }
+
+    // check if player runs into left, bottom or right canvas walls, prevent player from oving beyond wall boundaries
+    if (player.y > 383) {
+        player.y = 383;
+    }
+    if (player.x > 402.5) {
+        player.x = 402.5;
+    }
+    if (player.x < 2.5) {
+        player.x = 2.5;
+    }
+};
+
+//Increase the number of enemies on screen based on the player's score
+let increasesDifficulty = function(numEnemies) {
+    //remove all previous enemies on canvas
+    allEnemies.length = 0;
+
+    //load new set of enemies
+    for (let i = 0; i <= numEnemies; i++) {
+        let enemy = new Enemy(0, Math.random() * 184, Math.random() * 256);
+
+        allEnemies.push(enemy);
+    }
+};
+
+
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
+// Enemy randomly placed vertically within sections of canvas
+// Declare new score and gameLevel variables to store score and level
+let allEnemies = [];
+let player = new Player(202.5, 383, 50);
+let score  = 0;
+let gameLevel = 1;
+let scoreLevelDiv = document.createElement('div');
+let enemy = new Enemy(0, Math.random() * 184 + 50, Math.random() * 256);
 
+allEnemies.push(enemy);
 
 
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
 document.addEventListener('keyup', function(e) {
-    var allowedKeys = {
+    let allowedKeys = {
         37: 'left',
         38: 'up',
         39: 'right',
@@ -71,4 +155,5 @@ document.addEventListener('keyup', function(e) {
     };
 
     player.handleInput(allowedKeys[e.keyCode]);
+    console.log(allowedKeys[e.keyCode]);
 });
